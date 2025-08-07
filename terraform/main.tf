37817cc0961a5d02bde9a80a41458ea60b5aff90
Eduardo Garcia <eduardogar@gmail.com>
2025-08-07 14:29:03 -0600

    Adding git .gitignore

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@
 terraform {
   required_providers {
     minikube = {
-      source = "gavinbunney/minikube"
+      source = "scott-the-programmer/minikube"
     }
     docker = {
       source = "kreuzwerker/docker"
